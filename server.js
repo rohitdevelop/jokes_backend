f3dbc140ab2f26d7587deff9c14bd2ad3f8a5c8a
Rohit singh <rohitdev124421@gmail.com>
2025-04-24 15:02:01 +0530

    jai mat de

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors()); // allow frontend requests
 
